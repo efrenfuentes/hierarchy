9f4e13f451c35e51b299e36f6cd59a9cb91291ac
Efrén Fuentes <developer@efrenfuentes.net>
2025-02-09 13:40:49 -0400

    Initial commit

---
+++ lib/hierarchy.ex
@@ -0,0 +1,18 @@
+defmodule Hierarchy do
+  @moduledoc """
+  Documentation for `Hierarchy`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hierarchy.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hierarchy_test.exs
@@ -0,0 +1,8 @@
+defmodule HierarchyTest do
+  use ExUnit.Case
+  doctest Hierarchy
+
+  test "greets the world" do
+    assert Hierarchy.hello() == :world
+  end
+end
